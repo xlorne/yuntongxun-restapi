22ac148dccbfc1c932f9af6b56e579e30c287c24
wangliang <1991wangliang@gmail.com>
2017-09-25 14:40:57 +0800

    maven

---
+++ src/main/java/com/lorne/yuntongxun/YunTongXunConfiguration.java
@@ -0,0 +1,13 @@
+package com.lorne.yuntongxun;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * create by lorne on 2017/9/25
+ */
+@Configuration
+@ComponentScan
+public class YunTongXunConfiguration {
+
+}

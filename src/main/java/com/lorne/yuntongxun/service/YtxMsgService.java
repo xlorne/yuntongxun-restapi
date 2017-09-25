ab5c28850f288c1c62632190c4ad6f35c96bceab
wangliang <1991wangliang@gmail.com>
2017-09-25 14:28:06 +0800

    Initial commit

---
+++ src/main/java/com/lorne/yuntongxun/service/YtxMsgService.java
@@ -0,0 +1,13 @@
+package com.lorne.yuntongxun.service;
+
+/**
+ * 云通讯服务对象
+ * create by lorne on 2017/9/25
+ */
+public interface YtxMsgService {
+
+    void init();
+
+    boolean sendMsg(String msgId,String mobiles,String ... args);
+
+}
---
+++ src/main/java/com/lorne/yuntongxun/service/impl/YtxMsgServiceImpl.java
@@ -0,0 +1,44 @@
+package com.lorne.yuntongxun.service.impl;
+
+import com.cloopen.rest.sdk.CCPRestSDK;
+import com.lorne.core.framework.utils.config.ConfigUtils;
+import com.lorne.yuntongxun.service.YtxMsgService;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+/**
+ * create by lorne on 2017/9/25
+ */
+@Service
+public class YtxMsgServiceImpl implements YtxMsgService {
+
+    private CCPRestSDK restAPI;
+
+    @Override
+    public boolean sendMsg(String msgId, String mobiles, String... args) {
+        Map<String, Object> result = restAPI.sendTemplateSMS(mobiles,msgId,args);
+        if(result!=null&&"000000".equals(result.get("statusCode"))){
+            return true;
+        }
+        return false;
+    }
+
+
+    @Override
+    public void init() {
+        if(restAPI==null) {
+
+            restAPI = new CCPRestSDK();
+            String ip = ConfigUtils.getString("yuntongxun.properties","ip");
+            String port = ConfigUtils.getString("yuntongxun.properties","port");
+            String accountSid = ConfigUtils.getString("yuntongxun.properties","accountSid");
+            String accountToken = ConfigUtils.getString("yuntongxun.properties","accountToken");
+            String appId = ConfigUtils.getString("yuntongxun.properties","appId");
+
+            restAPI.init(ip, port);
+            restAPI.setAccount(accountSid, accountToken);
+            restAPI.setAppId(appId);
+        }
+    }
+}
